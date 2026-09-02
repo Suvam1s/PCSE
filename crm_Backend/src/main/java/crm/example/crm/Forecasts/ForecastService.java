99704a997d3f1d2df6fd3b26bf0f30cfab872d6d
Suvam1s <144379163+Suvam1s@users.noreply.github.com>
2026-09-02 10:35:18 +0000

    Forecast service added

--- crm_Backend/src/main/java/crm/example/crm/Forecasts/ForecastService.java
+++ crm_Backend/src/main/java/crm/example/crm/Forecasts/ForecastService.java
@@ -1,1 +1,46 @@
+package crm.example.crm.Forecasts;
+import java.util.*;
 
+import org.springframework.data.jpa.repository.*;
+
+@Service
+public class ForecastService {
+
+    private final ForecastsRepo forecastsrepo;
+    private final Accountsrepo accountsrepository1;
+
+    public ForecastService(ForecastsRepo forecastsrepo, Accountsrepo accountsrepository1) {
+        this.forecastsrepo = forecastsrepo;
+        this.accountsrepository1 = accountsrepository1;
+    }
+      
+    //SERVICE 1
+    public Forecasts addForecasts(
+            String OpportunityName,
+            String AccountName,
+            stage Stage,
+            Long amount,
+            LocalDate closeDate) {
+
+        Optional<Accounts> account = accountsrepository1.findByName(AccountName);
+
+        if (account.isEmpty()) {
+            throw new IllegalArgumentException("name not found");
+        }
+
+
+        if (forecastsrepo.existsByAccountAndOpportunityName(account.get(), OpportunityName)) {
+            throw new IllegalArgumentException("Opportunity already exists for this account");
+        } 
+
+        Forecasts forecast = new Forecasts(
+                OpportunityName,
+                account.get(),
+                Stage,
+                amount,
+                closeDate
+        );
+
+        return forecastsrepo.save(forecast);
+    }
+}
--- crm_Backend/src/main/java/crm/example/crm/Forecasts/ForecastsRepo.java
+++ crm_Backend/src/main/java/crm/example/crm/Forecasts/ForecastsRepo.java
@@ -2,6 +2,6 @@ package crm.example.crm.Forecasts;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.*;
-public interface ForecastsRepo extends JpaRepository(){
-    
+public interface Accountsrepo extends JpaRepository<Accounts, Long> {
+    boolean existsByAccountAndOpportunityName(Accounts account, String OpportunityName);
 }
